80f220300ba1a39f44e9737df00a9b6a76153147
Pelumiade <fecoyifemi@gmail.com>
2023-07-12 14:53:29 +0100

    mapx

--- MapX/settings.py
+++ MapX/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 from datetime import timedelta
 
 import os
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -98,13 +99,18 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -141,8 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = "MapX_project/static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR / 'expert_portal' / 'static')]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
